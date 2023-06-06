ba670e4a12284e2a8ae0f0c6f4ece60becc07480
kaseen <srbtrxtheinsane@gmail.com>
2023-06-06 18:13:39 +0200

    added script for frontend export

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,15 @@
 const hre = require('hardhat');
+const fs = require('fs');
 
 const main = async () => {
+
     const BlockJuice = await hre.ethers.getContractFactory('BlockJuice');
     const BlockJuiceContract = await BlockJuice.deploy(1234, '0x694AA1769357215DE4FAC081bf1f309aDC325306');
     await BlockJuiceContract.deployed();
 
-    console.log('Address:', BlockJuiceContract.address);
+    const rawData = JSON.parse(fs.readFileSync('./artifacts/contracts/BlockJuice.sol/BlockJuice.json', 'utf8'));
+    const res = JSON.stringify({ address: BlockJuiceContract.address, abi: rawData.abi });
+    fs.writeFileSync('./frontend.json', res);
 }
 
 main().catch((error) => {
